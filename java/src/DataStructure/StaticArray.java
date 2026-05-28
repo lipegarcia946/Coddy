ef7ff48db412325451457862032c9169621d62d6
lipegarcia946 <lipegarcia946@gmail.com>
2026-05-28 19:03:42 -0300

    Adicionando o arquivo StaticArray.java e o novo pacote DataStructure.

---
+++ java/src/DataStructure/StaticArray.java
@@ -0,0 +1,17 @@
+package DataStructure;
+
+public class StaticArray {
+    public static void main(String[] args) {
+        System.out.println("Shopping List:");
+        String[] shoppingList = {"brad", "eggs", "milk", "butter"};
+
+        for (int i = 0; i < shoppingList.length; i++) {
+            System.out.println(shoppingList[i]);
+//
+//        forma melhor para escrever essa lista
+//        for (String s : shoppingList) {
+//            System.out.println(s);
+//        }
+        }
+    }
+}
